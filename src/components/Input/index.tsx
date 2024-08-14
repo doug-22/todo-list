cd5d77cf3d7b647ea3082966973af873212c4ed1
caio-boos <caio.nerd.0@gmail.com>
2024-08-14 09:24:23 -0400

    subindo mais uma parte

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,66 @@
+import React, { forwardRef, LegacyRef } from "react";
+import { TextInput, View, TextInputProps, Text, TouchableOpacity} from 'react-native';
+import { MaterialIcons, FontAwesome, Octicons } from '@expo/vector-icons';
+import { themas } from "../../global/themes";
+import { style } from "./styles";
+
+type IconComponent = React.ComponentType<React.ComponentProps<typeof MaterialIcons>> | 
+                     React.ComponentType<React.ComponentProps<typeof FontAwesome>> | 
+                     React.ComponentType<React.ComponentProps<typeof Octicons>>;
+
+type Props = TextInputProps & {
+    IconLeft?: IconComponent,
+    IconRigth?: IconComponent,
+    iconLeftName?: string,  
+    iconRightName?: string, 
+    title?: string,
+    onIconLeftPress?: () => void, 
+    onIconRigthPress?: () => void 
+}
+
+export const Input = forwardRef((props: Props, ref: LegacyRef<TextInput> | null) => {
+    const { IconLeft, IconRigth, iconLeftName, iconRightName, title, onIconLeftPress, onIconRigthPress, ...rest } = props;
+
+    const calculateSizeWidth = () => {
+        if (IconLeft && IconRigth) {
+            return '80%';
+        } else if (IconLeft || IconRigth) {
+            return '90%';
+        } else {
+            return '100%';
+        }
+    };
+
+    const calculateSizePaddingLeft = () => {
+        if (IconLeft && IconRigth) {
+            return 0;
+        } else if (IconLeft || IconRigth) {
+            return 10;
+        } else {
+            return 20;
+        }
+    };
+
+    return (
+        <>
+            {title && <Text style={style.titleInput}>{title}</Text>}
+            <View style={[style.boxInput, { paddingLeft: calculateSizePaddingLeft() }]}>
+                {IconLeft && iconLeftName && (
+                    <TouchableOpacity onPress={onIconLeftPress} style={style.Button}>
+                        <IconLeft name={iconLeftName as any} size={20} color={themas.Colors.gray} style={style.Icon} />
+                    </TouchableOpacity>
+                )}
+                <TextInput 
+                    {...rest}
+                    style={[style.input, { width: calculateSizeWidth() }]}
+                    ref={ref}
+                />
+                {IconRigth && iconRightName && (
+                    <TouchableOpacity onPress={onIconRigthPress} style={style.Button}>
+                        <IconRigth name={iconRightName as any} size={20} color={themas.Colors.gray} style={style.Icon} />
+                    </TouchableOpacity>
+                )}
+            </View>
+        </>
+    );
+});
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,17 +1,21 @@
 import React,{ useState } from "react";
 import { style } from "./styles";
 import Logo from '../../assets/logo.png'
-import { themas } from "../../global/themes";
- import {MaterialIcons} from '@expo/vector-icons';
-import {Text, View,Image,TextInput, TouchableOpacity, Alert} from 'react-native'
+import {MaterialIcons,Octicons} from '@expo/vector-icons';
+import {Text, View,Image,TouchableOpacity, Alert} from 'react-native'
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
 export default function Login (){
 
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
+    const [showPassword,setShowPassword] = useState(true);
+    const [loading,setLoading] = useState(false)
 
 
     async function getLogin() {
+        setLoading(true)
         try {
             
             if(!email ||!password){
@@ -19,10 +23,11 @@ export default function Login (){
             }
 
             console.log('LOGOU!')
-
+           
         } catch (error) {
             console.log(error)
         }
+        setLoading(false)
     }
 
 
@@ -37,37 +42,26 @@ export default function Login (){
                 <Text style={style.text}>Bem vindo de volta!</Text>
             </View>
             <View style={style.boxMid}>
-                <Text style={style.titleInput}>ENDEREÇO E-MAIL</Text>
-                <View style={style.boxInput}>
-                    <TextInput 
-                        style={style.input}
-                        onChangeText={setEmail}
-                        value={email}
-                    />
-                    <MaterialIcons 
-                        name="email"
-                        size={20}
-                        color={themas.Colors.gray}
-                    />
-                </View>
-                <Text style={style.titleInput}>ENDEREÇO E-MAIL</Text>
-                <View style={style.boxInput}>
-                    <TextInput 
-                        style={style.input}
-                        onChangeText={setPassword}
-                        value={password}
-                    />
-                    <MaterialIcons 
-                        name="remove-red-eye"
-                        size={20}
-                        color={themas.Colors.gray}
-                    />
-                </View>
+                <Input 
+                    title="ENDEREÇO E-MAIL"
+                    value={email}
+                    onChangeText={setEmail}
+                    IconRigth={MaterialIcons}
+                    iconRightName="email"
+                    onIconRigthPress={()=>console.log('OLA')}
+                />
+                <Input 
+                    title="SENHA"
+                    value={password}
+                    onChangeText={setPassword}
+                    IconRigth={Octicons}
+                    iconRightName={showPassword?"eye-closed":"eye"}
+                    onIconRigthPress={()=>setShowPassword(!showPassword)}
+                    secureTextEntry={showPassword}
+                />
             </View>
             <View style={style.boxBottom}>
-                <TouchableOpacity style={style.button} onPress={()=>getLogin()}>
-                    <Text style={style.textButton}>Entrar</Text>
-                </TouchableOpacity>
+                <Button  text="ENTRAR" loading={loading}/>
             </View>
             <Text style={style.textBottom}>Não tem conta? <Text  style={style.textBottomCreate}>Crie agora</Text></Text>
         </View>
